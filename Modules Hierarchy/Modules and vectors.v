28deb5a07c9d6d5564d9b3380635efbee22ccc7c
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-05 09:36:43 +0530

    Create Modules and vectors.v

---
+++ Modules Hierarchy/Modules and vectors.v
@@ -0,0 +1,19 @@
+module top_module ( 
+    input clk, 
+    input [7:0] d, 
+    input [1:0] sel, 
+    output [7:0] q 
+);
+    wire [7:0]a,b,c;
+    my_dff8(clk,d,a);
+    my_dff8(clk,a,b);
+    my_dff8(clk,b,c);
+    always@(*)begin
+        case (sel)
+            2'b00:q=d;
+            2'b01:q=a;
+            2'b10:q=b;
+            2'b11:q=c;
+        endcase
+     end
+endmodule
