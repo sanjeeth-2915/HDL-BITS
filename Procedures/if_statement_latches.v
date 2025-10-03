9f86f65ec4bcd5cbfa4620b784676c67cfa0435a
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-10-03 11:47:12 +0530

    Create if_statement_latches.v

---
+++ Procedures/if_statement_latches.v
@@ -0,0 +1,20 @@
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        shut_off_computer = 0;
+        if (cpu_overheated)
+           shut_off_computer = 1;
+    end
+
+    always @(*) begin
+        keep_driving = 0;
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+    end
+
+endmodule
