423970debf6e322f31830c5deda6bacc5f42ad86
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-10-04 14:29:27 +0530

    Update Conditional_ternary_operator.v

--- Verilog Language/Moreverilog Features/Conditional_ternary_operator.v
+++ Verilog Language/Moreverilog Features/Conditional_ternary_operator.v
@@ -1,1 +1,10 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+    wire [7:0] x,y;
+    
+    assign x=a<b?a:b;
+    assign y=x<c?x:c;
+    assign min = y<d?y:d;
 
+endmodule
